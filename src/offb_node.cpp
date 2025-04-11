390f1234b1656e798368ad124c83b52ff03c7c39
MatCat960 <mattia.catellani@unimore.it>
2025-04-11 14:34:01 +0200

    publish position setpoint before takeoff

--- src/offb_node.cpp
+++ src/offb_node.cpp
@@ -9,10 +9,22 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <nav_msgs/Odometry.h>
 
 mavros_msgs::State current_state;
+nav_msgs::Odometry odom;
+bool odom_received = false;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
+}
+
+void odom_cb(const nav_msgs::Odometry::ConstPtr& msg){
+	odom = *msg;
+	if (odom.pose.pose.position.z != 0.0){
+		odom_received = true;
+		std::cout << "odom: " << odom.pose.pose.position.x << ", " << odom.pose.pose.position.y << ", " << odom.pose.pose.position.z << std::endl;
+	}
 }
 
 int main(int argc, char **argv)
@@ -28,27 +40,48 @@ int main(int argc, char **argv)
             ("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::TwistStamped>
+	    ("mavros/setpoint_velocity/cmd_vel", 10);
+
+    ros::Subscriber odom_pub = nh.subscribe<nav_msgs::Odometry>
+	    ("mavros/local_position/odom", 10, odom_cb);
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
 
     // wait for FCU connection
-    while(ros::ok() && !current_state.connected){
+    while(ros::ok() && !current_state.connected && !odom_received){
         ros::spinOnce();
         rate.sleep();
+	std::cout << "Spinning...\n";
     }
+    // while(ros::ok() && !odom_received){
+    //	std::cout << "Waiting 5 seconds...\n";
+    //	ros::Duration(5.0).sleep();
+    //}
+    geometry_msgs::PoseStamped start_pose;
+    start_pose.pose.position.x = odom.pose.pose.position.x;
+    start_pose.pose.position.y = odom.pose.pose.position.y;
+    start_pose.pose.position.z = odom.pose.pose.position.z;
+    std::cout << "Starting pos: " << start_pose.pose.position.x << ", " << start_pose.pose.position.y << ", " << start_pose.pose.position.z << "\n";
 
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
-
+    geometry_msgs::PoseStamped takeoff_pose;
+    takeoff_pose.pose.position.x = start_pose.pose.position.x;
+    takeoff_pose.pose.position.y = start_pose.pose.position.y;
+    takeoff_pose.pose.position.z = start_pose.pose.position.z + 2.0; 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
+        local_pos_pub.publish(start_pose);
         ros::spinOnce();
         rate.sleep();
     }
+
+    geometry_msgs::TwistStamped vel;
+    vel.twist.linear.x = 0.0;
+    vel.twist.linear.y = 0.0;
+    vel.twist.linear.z = 0.0;
+    vel_pub.publish(vel);
+
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
@@ -59,9 +92,12 @@ int main(int argc, char **argv)
     ros::Time last_request = ros::Time::now();
 
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
+	    
+            	//vel_pub.publish(vel);
+	    if( current_state.mode != "OFFBOARD" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
+            
+		if( set_mode_client.call(offb_set_mode) &&
                 offb_set_mode.response.mode_sent){
                 ROS_INFO("Offboard enabled");
             }
@@ -77,7 +113,8 @@ int main(int argc, char **argv)
             }
         }
 
-        local_pos_pub.publish(pose);
+	//std::cout << "Taking off ... \n";
+	local_pos_pub.publish(takeoff_pose);
 
         ros::spinOnce();
         rate.sleep();
